e3e5e543b7703612acf80ec64bf3b5a676c9359b
Adrien <adrien.devaux2@laposte.net>
2019-03-07 22:44:11 +0100

    ihofa

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
-config.active_record.migration_error = false#:page_load
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.active_record.migration_error = false#:page_load
