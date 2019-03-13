00da91ca7042cf54798b84c483b329968e079802
Adrien <adrien.devaux2@laposte.net>
2019-03-13 13:43:45 +0100

    ihofa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_094919) do
+ActiveRecord::Schema.define(version: 2019_03_13_103223) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "prix"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
